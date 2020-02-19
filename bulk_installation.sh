cd12acb25d1c0f6976caae552121f49e0231c4c7
Maria Luiza Linhares Dantas <mlldantas@pm.me>
2020-02-19 18:27:13 -0300

    testing

---
+++ bulk_installation.sh
@@ -0,0 +1,9 @@
+# All the stuff I need to reinstall after formatting the computer (or setting up a new one)
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get autoremove
+
+sudo apt-get install exfat-fuse exfat-utils  # these are important to mount large flash drives, SD cards, and so on.
+sudo apt-get install htop
+sudo apt-get install keepassx
