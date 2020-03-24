6bfc9f3de468c8e358658f9d08aec19a3fa55588
mdastro <mlldantas@pm.me>
2020-03-24 12:33:21 -0300

    :ok_hand: testing

--- bulk_installation.sh
+++ bulk_installation.sh
@@ -8,10 +8,14 @@ sudo apt-get install exfat-fuse exfat-utils  # these are important to mount larg
 sudo apt-get install htop
 sudo apt-get install keepassx
 sudo apt-get install r-base
+sudo apt-get install xclip                   # for copying and pasting through terminal
+sudo apt install default-jre 
+sudo apt install openjdk-11-jre-headless
+#sudo apt install openjdk-8-jre-headless
 
 # to do: add installations that require downloading files with wget
 
 # installing and confguring git
 sudo apt-get install git-all
-git config --global user.name "M.L.L.DANTAS"
-git config --global user.email "mlldantas@protonmail.com"
+git config --global user.name "mdastro"
+git config --global user.email "mlldantas@pm.me"
