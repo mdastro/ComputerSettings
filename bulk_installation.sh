b4546eb199b989156f2fe11494b8b16e797f9d37
mdastro <mlldantas@pm.me>
2020-04-28 11:33:48 -0300

    :art: improving automation

--- bulk_installation.sh
+++ bulk_installation.sh
@@ -1,22 +1,23 @@
 # All the stuff I need to reinstall after formatting the computer (or setting up a new one)
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
+sudo apt-get update --assume-yes
+sudo apt-get upgrade --assume-yes
+sudo apt-get autoremove --assume-yes
 
-sudo apt-get install exfat-fuse exfat-utils  # these are important to mount large flash drives, SD cards, and so on.
-sudo apt-get install htop
-sudo apt-get install keepassx
-sudo apt-get install r-base
-sudo apt-get install xclip                   # for copying and pasting through terminal
-sudo apt install default-jre 
-sudo apt install openjdk-11-jre-headless
-sudo apt install hplip                       # hp printer/scanner set up
-#sudo apt install openjdk-8-jre-headless
+# the following packages ('exfat-fuse', etc.) are important to mount large flash drives, SD cards, and so on.
+sudo apt-get install exfat-fuse exfat-utils --assume-yes 
+sudo apt-get install htop --assume-yes
+sudo apt-get install keepassx --assume-yes
+sudo apt-get install r-base --assume-yes
+sudo apt-get install xclip --assume-yes    # for copying and pasting through terminal
+sudo apt install default-jre --assume-yes
+sudo apt install openjdk-11-jre-headless --assume-yes
+sudo apt install hplip --assume-yes        # hp printer/scanner set up
+#sudo apt install openjdk-8-jre-headless --assume-yes
 
 # to do: add installations that require downloading files with wget
 
 # installing and confguring git
-sudo apt-get install git-all
+sudo apt-get install git-all --assume-yes
 git config --global user.name "mdastro"
 git config --global user.email "mlldantas@pm.me"
---
+++ python_packages_via_conda.sh
@@ -0,0 +1,26 @@
+# python packages installation after conda installation
+
+conda install pip
+conda install jupyter
+
+# general packages
+conda install --yes numpy
+conda install --yes pandas
+conda install --yes matplotlib
+conda install --yes seaborn
+
+# stats and scientific packages
+conda install --yes scipy
+conda install --yes cython
+conda install --yes statsmodels
+conda install --yes pymc
+conda install --yes pystan
+conda install --yes scikit-learn
+conda install --yes arviz
+
+# astro packages
+conda install --yes astropy
+conda install --yes pyneb
+conda install --yes astroquery
+
+conda update --yes --all
